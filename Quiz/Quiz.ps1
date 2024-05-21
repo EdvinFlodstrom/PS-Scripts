2b79ee6531b27290da0c3a2ea3395687b1ca9f7b
Edvin Flodström <edvin.flodstrom@gmail.com>
2024-05-21 18:43:06 +0200

    feat(): Add script to open React-Quiz

---
+++ Quiz/Quiz.ps1
@@ -0,0 +1,21 @@
+﻿$ps1Path = "C:\Path1"
+$ps2Path = "C:\Path2"
+$ps3Path = "C:\Path3"
+
+$backendLocationCommand = "Set-Location -Path \`"$ps2Path\`""
+$frontendLocationCommand = "Set-Location -Path \`"$ps3Path\`""
+
+function StartProcessArguments {
+    param (
+        [string]$locationCommand
+    )
+    "-NoExit",
+    "-Command",
+    $locationCommand
+}
+
+Set-Location -Path $ps1Path
+
+Start-Process -FilePath "powershell.exe" -ArgumentList (StartProcessArguments -locationCommand $backendLocationCommand) -Verb RunAs
+
+Start-Process -FilePath "powershell.exe" -ArgumentList (StartProcessArguments -locationCommand $frontendLocationCommand) -Verb RunAs
